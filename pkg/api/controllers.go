3f87ff3338f77f152afa332c7a5fda53e02ea36f
NeuralNinjaTheSecond <neuralninja@tensorplex.ai>
2024-04-26 00:35:28 +0800

    finally figured out static types and added better validation

--- pkg/api/controllers.go
+++ pkg/api/controllers.go
@@ -3,18 +3,23 @@ package api
 import (
 	"dojo-api/pkg/task"
 	"dojo-api/utils"
+	// "encoding/json"
+	"errors"
 	"fmt"
+	// "reflect"
+	// "time"
 	"github.com/gin-gonic/gin"
+	// "github.com/mitchellh/mapstructure"
 )
 
 func CreateTaskController(c *gin.Context) {
-	var requestBody map[string]interface{}
+	var requestBody utils.TaskRequest
 	var logger = utils.GetLogger()
 	response := make(map[string]interface{})
+	response["success"] = false
+	response["body"] = nil
 	networkUserId, exists := c.Get("networkUserID")
 	if !exists {
-		response["success"] = false
-		response["body"] = nil
 		response["error"] = "Unauthorized"
 		c.JSON(401, response)
 		return
@@ -23,17 +28,21 @@ func CreateTaskController(c *gin.Context) {
 	if err := c.BindJSON(&requestBody); err != nil {
 		// DO SOMETHING WITH THE ERROR
 		logger.Error().Msg(fmt.Sprintf("Error binding request body: %v", err))
-		response["success"] = false
-		response["body"] = nil
 		response["error"] = fmt.Sprintf("Error binding request body: %v", err)
 		c.JSON(400, response)
 	}
+
+	err := validateTaskRequest(requestBody)
+	if err != nil {
+		response["error"] = fmt.Sprintf("Error validating request: %v", err)
+		c.JSON(400, response)
+		return
+	}
+
 	taskService := task.NewTaskService()
 	msg, err := taskService.CreateTask(requestBody, networkUserId.(string))
 
 	if err != nil {
-		response["success"] = false
-		response["body"] = nil
 		response["error"] = fmt.Sprintf("Error creating task: %v", err)
 		c.JSON(500, response)
 		return
@@ -45,4 +54,111 @@ func CreateTaskController(c *gin.Context) {
 	c.JSON(200, response)
 }
 
+func validateTaskRequest(taskData utils.TaskRequest) error {
+	if taskData.Title == "" {
+		return errors.New("title is required")
+	}
 
+	if taskData.Body == "" {
+		return errors.New("body is required")
+	}
+
+	if taskData.ExpireAt == "" {
+		return errors.New("expireAt is required")
+	}
+
+	for _, taskInterface := range taskData.TaskData {
+		err := validateTaskData(taskInterface)
+		if err != nil {
+			return err
+		}
+	}
+
+	if taskData.MaxResults == 0 {
+		return errors.New("maxResults is required")
+	}
+
+	if taskData.TotalRewards == 0 {
+		return errors.New("totalRewards is required")
+	}
+
+	return nil
+}
+
+func validateTaskData(taskData utils.TaskData) error {
+	if taskData.Prompt == "" && len(taskData.Dialogue) == 0{
+		return errors.New("prompt is required")
+	}
+
+	if taskData.Task == "" {
+		return errors.New("task is required")
+	}
+	task := taskData.Task
+	if task == "CODE_GENERATION" || task == "TEXT_TO_IMAGE" {
+		for _, taskresponse := range taskData.Responses {
+			var ok bool
+			if task == "CODE_GENERATION" {
+				fmt.Println(taskresponse.Completion)
+				_, ok = taskresponse.Completion.(map[string]interface{})
+			} else if task == "TEXT_TO_IMAGE" {
+				_, ok = taskresponse.Completion.(string)
+			}
+			if !ok {
+				return fmt.Errorf("invalid completion format: %v", taskresponse.Completion)
+			}
+		}
+
+		if len(taskData.Dialogue) != 0 {
+			return errors.New("dialogue should be empty for code generation and text to image tasks")
+		}
+	// TODO: change to dialogue when schema is updated
+	} else if task == "CONVERSATION" {
+		if len(taskData.Responses) != 0 {
+			return errors.New("responses should be empty for dialogue task")
+		}
+
+		if len(taskData.Dialogue) == 0 {
+			return errors.New("dialogue is required for dialogue task")
+		}
+	}else {
+		return errors.New("invalid task")
+	}
+
+	if len(taskData.Criteria) == 0 {
+		return errors.New("criteria is required")
+	}
+
+	for _, criteria := range taskData.Criteria {
+		if criteria.Type == "" {
+			return errors.New("type is required for criteria")
+		}
+
+		if criteria.Type == "multi-select" || criteria.Type == "ranking" {
+			if len(criteria.Options) == 0 {
+				return errors.New("options is required for multiple choice criteria")
+			}
+		} else if criteria.Type == "score" {
+			if criteria.Min == 0 && criteria.Max == 0 {
+				return errors.New("min or max is required for numeric criteria")
+			}
+		}
+	}
+
+	return nil
+}
+
+// func parseCompletion(completion interface{}) (interface{}, error) {
+// 	switch completion := completion.(type) {
+// 	case string:
+// 		return completion, nil
+// 	case map[string]interface{}:
+// 		var completionStruct utils.Completion
+// 		err := mapstructure.Decode(completion, &completionStruct)
+// 		if err != nil {
+// 			return nil, errors.New("error decoding completion")
+// 		}
+// 		return completionStruct, nil
+// 	default:
+// 		return nil, errors.New("invalid completion format")
+// 	}
+// }
