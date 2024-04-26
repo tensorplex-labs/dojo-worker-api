0649376cfed9a9e8126cb090389b504b32e7c580
codebender <167290009+codebender37@users.noreply.github.com>
2024-04-26 12:42:56 +0700

    feat: updated code along with data schema

--- pkg/task/model/task.go
+++ pkg/task/model/task.go
@@ -7,13 +7,26 @@ import (
 
 // TaskResponse reflects the task structure used in API responses
 type TaskResponse struct {
-	ID         string          `json:"taskId"`
-	Title      string          `json:"title"`
-	Body       string          `json:"body"`
-	Modality   db.TaskModality `json:"modality"`
-	ExpireAt   time.Time       `json:"expireAt"`
-	Criteria   db.JSON         `json:"criteria"`
-	TaskData   db.JSON         `json:"taskData"`
-	Status     db.TaskStatus   `json:"status"`
-	MaxResults int             `json:"maxResults"`
+	ID         string        `json:"taskId"`
+	Title      string        `json:"title"`
+	Body       string        `json:"body"`
+	ExpireAt   time.Time     `json:"expireAt"`
+	Type       db.TaskType   `json:"type"`
+	TaskData   db.JSON       `json:"taskData"`
+	Status     db.TaskStatus `json:"status"`
+	MaxResults int           `json:"maxResults"`
 }
+
+// {
+// 	"success": true,
+// 	"body": {
+// 	  "taskId": "123",
+// 	  "title": "Task Title",
+// 	  "body": "Detailed task description",
+// 	  "expireAt": "YYYY-MM-DD HH:MM:SS",
+// 	  "taskData": [....],
+// 	  "status": "Pending",
+// 	  "maxResults": 10
+// 	},
+// 	"error": null
+//   }
