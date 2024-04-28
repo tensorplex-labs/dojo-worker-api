cb8e8af60c4ca9a1654339f0970255f1ae50ba6a
jarvis8x7b <157810922+jarvis8x7b@users.noreply.github.com>
2024-04-28 16:45:45 +0800

    refactor: rename to ORM for clarity, add get by id

--- pkg/orm/dojo_worker.go
+++ pkg/orm/dojo_worker.go
@@ -2,22 +2,23 @@ package orm
 
 import (
 	"context"
-	"dojo-api/db"
 	"errors"
+
+	"dojo-api/db"
 
 	"github.com/rs/zerolog/log"
 )
 
-type DojoWorkerService struct {
+type DojoWorkerORM struct {
 	dbClient *db.PrismaClient
 }
 
-func NewDojoWorkerService() *DojoWorkerService {
+func NewDojoWorkerORM() *DojoWorkerORM {
 	client := NewPrismaClient()
-	return &DojoWorkerService{dbClient: client}
+	return &DojoWorkerORM{dbClient: client}
 }
 
-func (s *DojoWorkerService) CreateDojoWorker(walletAddress string, chainId string) (*db.DojoWorkerModel, error) {
+func (s *DojoWorkerORM) CreateDojoWorker(walletAddress string, chainId string) (*db.DojoWorkerModel, error) {
 	ctx := context.Background()
 	worker, err := s.dbClient.DojoWorker.CreateOne(
 		db.DojoWorker.WalletAddress.Set(walletAddress),
@@ -26,12 +27,25 @@ func (s *DojoWorkerService) CreateDojoWorker(walletAddress string, chainId strin
 	return worker, err
 }
 
-func (s *DojoWorkerService) GetDojoWorkerByWalletAddress(walletAddress string) (*db.DojoWorkerModel, error) {
+func (s *DojoWorkerORM) GetById(ctx context.Context, workerId string) (*db.DojoWorkerModel, error) {
+	worker, err := s.dbClient.DojoWorker.FindUnique(
+		db.DojoWorker.ID.Equals(workerId),
+	).Exec(ctx)
+	if err != nil {
+		if errors.Is(err, db.ErrNotFound) {
+			log.Error().Err(err).Msg("Worker not found")
+			return nil, err
+		}
+		return nil, err
+	}
+	return worker, nil
+}
+
+func (s *DojoWorkerORM) GetDojoWorkerByWalletAddress(walletAddress string) (*db.DojoWorkerModel, error) {
 	ctx := context.Background()
 	worker, err := s.dbClient.DojoWorker.FindFirst(
 		db.DojoWorker.WalletAddress.Equals(walletAddress),
 	).Exec(ctx)
-
 	if err != nil {
 		if errors.Is(err, db.ErrNotFound) {
 			log.Error().Err(err).Msg("Worker not found")
