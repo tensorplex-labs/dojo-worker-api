09f3c3176ddad41c3eebcf6590a747534d94afca
jarvis8x7b <157810922+jarvis8x7b@users.noreply.github.com>
2024-04-29 23:47:36 +0800

    fix: display correct numResults from submit task API

--- pkg/orm/task_result.go
+++ pkg/orm/task_result.go
@@ -19,6 +19,7 @@ func NewTaskResultORM() *TaskResultORM {
 // In a transaction creates the TaskResult and updates the Task.NumResults
 func (t *TaskResultORM) CreateTaskResult(ctx context.Context, taskResult *db.InnerTaskResult) (*db.TaskResultModel, error) {
 	// TODO add web3 integration fields when the time comes
+	updateTaskTx := t.client.Task.FindUnique(db.Task.ID.Equals(taskResult.TaskID)).Update(db.Task.NumResults.Increment(1)).Tx()
 	createResultTx := t.client.TaskResult.CreateOne(
 		db.TaskResult.Status.Set(taskResult.Status),
 		db.TaskResult.ResultData.Set(taskResult.ResultData),
@@ -31,9 +32,7 @@ func (t *TaskResultORM) CreateTaskResult(ctx context.Context, taskResult *db.Inn
 	).With(
 		db.TaskResult.Task.Fetch(),
 	).Tx()
-	updateTaskTx := t.client.Task.FindUnique(db.Task.ID.Equals(taskResult.TaskID)).Update(db.Task.NumResults.Increment(1)).Tx()
-
-	if err := t.client.Prisma.Transaction(createResultTx, updateTaskTx).Exec(ctx); err != nil {
+	if err := t.client.Prisma.Transaction(updateTaskTx, createResultTx).Exec(ctx); err != nil {
 		return nil, err
 	}
 	return createResultTx.Result(), nil
