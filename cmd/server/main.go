726d37eedd694b7721bfa57aef40d311897d5761
jarvis8x7b <157810922+jarvis8x7b@users.noreply.github.com>
2024-05-01 18:58:45 +0700

    add cors to allow all origins

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,6 +5,7 @@ import (
 	"dojo-api/utils"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
 	"github.com/joho/godotenv"
 	"github.com/rs/zerolog/log"
 )
@@ -18,6 +19,7 @@ func main() {
 	port := utils.LoadDotEnv("SERVER_PORT")
 
 	r := gin.Default()
+	r.Use(cors.Default())
 	api.LoginRoutes(r)
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
