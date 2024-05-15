382bfcb93d6cf22af1b1579cae81589de9c81d4c
codebender <167290009+codebender37@users.noreply.github.com>
2024-05-15 15:14:28 +0700

    feat: implemented file upload to S3 in create-task api

--- utils/utils.go
+++ utils/utils.go
@@ -1,14 +1,20 @@
 package utils
 
 import (
+	"context"
 	"crypto/rand"
 	"encoding/hex"
 	"flag"
 	"fmt"
+	"mime/multipart"
 	"os"
 	"strconv"
 	"time"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/joho/godotenv"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
@@ -40,6 +46,10 @@ func init() {
 	LoadDotEnv("TOKEN_EXPIRY")
 	LoadDotEnv("SERVER_PORT")
 	LoadDotEnv("ETHEREUM_NODE")
+	// TODO - maybe move under aws runtime env
+	LoadDotEnv("AWS_S3_BUCKET_NAME")
+	LoadDotEnv("AWS_ACCESS_KEY_ID")
+	LoadDotEnv("AWS_SECRET_ACCESS_KEY")
 
 	zerolog.ErrorStackMarshaler = pkgerrors.MarshalStack
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
@@ -106,4 +116,56 @@ func GenerateRandomMinerSubscriptionKey() (string, error) {
 	key := hex.EncodeToString(b)
 	key = "sk-" + key
 	return key, nil
+}
+
+// Initialize the S3 client
+func getS3Client() (*s3.Client, error) {
+	// Load the default AWS configuration
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		return nil, err
+	}
+	// Create an S3 client using the loaded configuration
+	client := s3.NewFromConfig(cfg)
+	return client, nil
+}
+
+// Get the S3 uploader
+func getS3Uploader(client *s3.Client) *manager.Uploader {
+	return manager.NewUploader(client)
+}
+
+func UploadFileToS3(file *multipart.FileHeader) (*manager.UploadOutput, error) {
+	// Open the file
+	bucketName := LoadDotEnv("AWS_S3_BUCKET_NAME")
+	src, err := file.Open()
+	if err != nil {
+		return nil, err
+	}
+	defer src.Close()
+	// Generate a unique file name for the S3 object
+	// fileName := fmt.Sprintf("uploads/%d_%s", time.Now().Unix(), file.Filename)
+
+	// Create an S3 client
+	log.Info().Interface("file", file).Msg("Uploading file")
+	s3Client, err := getS3Client()
+	if err != nil {
+		log.Error().Err(err).Msg("Error creating S3 client")
+		return nil, err
+	}
+	uploader := getS3Uploader(s3Client)
+
+	// Upload the file to S3
+	result, err := uploader.Upload(context.TODO(), &s3.PutObjectInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(file.Filename),
+		Body:   src,
+		// ContentType: aws.String(file.Header.Get("Content-Type")),
+	})
+	if err != nil {
+		log.Error().Err(err).Msg("Error uploading file")
+		return result, err
+	}
+
+	return result, nil
 }
