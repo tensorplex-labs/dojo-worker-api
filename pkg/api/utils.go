584deb44219ed3b7e63a1751cdf567c67b493287
codebender <167290009+codebender37@users.noreply.github.com>
2024-12-11 22:22:06 +0700

    fix: cache the last ip in X-Original-Forwarded for rate-limiting (#63)

--- pkg/api/utils.go
+++ pkg/api/utils.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"io"
 	"os"
+	"strings"
 	"time"
 
 	"dojo-api/db"
@@ -114,11 +115,18 @@ func handleMetricData(currentTask *db.TaskModel, updatedTask *db.TaskModel) {
 
 // Get the user's IP address from the gin request headers
 func getCallerIP(c *gin.Context) string {
-	// TODO - Need to check if this is the correct way without getting spoofing
 	if runtimeEnv := utils.LoadDotEnv("RUNTIME_ENV"); runtimeEnv == "aws" {
-		callerIp := c.Request.Header.Get("X-Original-Forwarded-For")
-		log.Debug().Msgf("Got caller IP from X-Original-Forwarded-For header: %s", callerIp)
-		return callerIp
+		forwardedFor := c.Request.Header.Get("X-Original-Forwarded-For")
+		if forwardedFor != "" {
+			// Split the string by comma and get the last IP
+			ips := strings.Split(forwardedFor, ",")
+			if len(ips) > 0 {
+				// Trim any whitespace from the last IP
+				lastIP := strings.TrimSpace(ips[len(ips)-1])
+				log.Debug().Msgf("Got last caller IP from X-Original-Forwarded-For header: %s", lastIP)
+				return lastIP
+			}
+		}
 	}
 	callerIp := c.ClientIP()
 	log.Debug().Msgf("Got caller IP from ClientIP: %s", callerIp)
