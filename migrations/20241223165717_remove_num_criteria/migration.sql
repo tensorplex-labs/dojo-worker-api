c9c160dd5695433b0f2f20be4089be1e0ad20fce
codebender <167290009+codebender37@users.noreply.github.com>
2024-12-23 23:40:16 +0630

    feat: added schema migration script with prisma migrate

---
+++ migrations/20241223165717_remove_num_criteria/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `num_criteria` on the `Task` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Task" DROP COLUMN "num_criteria";
