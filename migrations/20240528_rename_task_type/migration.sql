4601bd09daad20f353bc9822d7ea9c7998ae7799
karootplx <karoo@tensorplex.ai>
2025-04-06 23:46:03 +0800

    chore: update migration script and cleanup

--- migrations/20240528_rename_task_type/migration.sql
+++ migrations/20240528_rename_task_type/migration.sql
@@ -1,5 +1,6 @@
 -- Create new enum type
 CREATE TYPE "TaskModality" AS ENUM ('CODE_GENERATION', 'IMAGE', 'THREE_D');
+CREATE TYPE "TaskType" AS ENUM ('CODE_GENERATION', 'DIALOGUE', 'TEXT_TO_IMAGE', 'TEXT_TO_THREE_D');
 
 -- Delete all tasks with type 'DIALOGUE' as we don't want to migrate them
 DELETE FROM "Task" WHERE "type" = 'DIALOGUE'::"TaskType";
@@ -15,7 +16,11 @@ UPDATE "Task" SET "modality" = 'IMAGE'::"TaskModality" WHERE "type" = 'TEXT_TO_I
 -- Map 'TEXT_TO_THREE_D' to 'THREE_D'
 UPDATE "Task" SET "modality" = 'THREE_D'::"TaskModality" WHERE "type" = 'TEXT_TO_THREE_D'::"TaskType";
 
--- Make the new column NOT NULL after we've migrated all data
+-- Ensure all tasks have a valid modality value since the schema requires it to be NOT NULL
+-- We'll set a default value of CODE_GENERATION for all NULL values
+UPDATE "Task" SET "modality" = 'CODE_GENERATION'::"TaskModality" WHERE "modality" IS NULL;
+
+-- Make the modality column NOT NULL
 ALTER TABLE "Task" ALTER COLUMN "modality" SET NOT NULL;
 
 -- Drop the old column
